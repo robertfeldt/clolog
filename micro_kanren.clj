ea4f92ec688ec994989862f26b06a46a9f246d7e
Robert Feldt <robert.feldt@gmail.com>
2008-04-26 06:30:30 +0200

    finished the code on substitutions

--- micro_kanren.clj
+++ micro_kanren.clj
@@ -36,10 +36,53 @@
 
 ; Point 2: (Prolog-like) Logic variables
 
-; We represent logic variables with Clojure vectors
-(defn var [name] [name])
-(def var? vector?)
+; We represent logic variables with Clojure struct-maps
+(defstruct logical-var :name)
+(defn lvar [name] (struct logical-var name))
+(defn lvar? [v] (and (= clojure.lang.PersistentStructMap (class v)) (:name v)))
 
 ; We implement associations of logic variables and their values 
-; (aka substitutions) as Clojure maps.
-(def empty-subst {})
+; (aka substitutions) with Clojure maps.
+(defn empty-subst [] {})
+(defn ext-s [var value substs] (assoc substs var value))
+
+; Find the value associated with var in substitution s.
+; Return var itself if it is unbound.
+; In miniKanren, this function is called 'walk'
+(defn lookup [var s]
+  (cond
+    (not (lvar? var)) var
+    (contains? s var) (lookup (get s var) s)
+    true var)) ;; no else??
+
+(defn pair? [v] (and (vector? v) (= 2 (count v))))
+
+(defn unify [t1 t2 s]
+  (let [t1 (lookup t1 s)
+        t2 (lookup t2 s)]
+    (cond
+      (= t1 t2) s                  ; t1 and t2 the same, no new knowledge
+      (lvar? t1) (ext-s t1 t2 s)   ; t1 unbound
+      (lvar? t2) (ext-s t2 t1 s)   ; t2 unbound
+      (and (pair? t1) (pair? t2))  ; if t1 is a pair so is t2
+        (let [s (unify (first t1) (first t2) s)]
+          (and s (unify (frest t1) (frest t2) s)))
+      (identical? t1 t2) s       ; t1 and t2 are the same value
+      true false)))
+
+(def vx (lvar 'x))
+(def vy (lvar 'y))
+(def vz (lvar 'z))
+(def vq (lvar 'q))
+
+(println (unify vx vy (empty-subst)))
+(println (unify vx 1 (unify vx vy (empty-subst))))
+(println (lookup vy (unify vx 1 (unify vx vy (empty-subst)))))
+(println (unify [vx vy] [vy 1] (empty-subst)))
+
+
+; Part 3: Logic system
+;
+; Now we can combine non-deterministic functions (part 1) and
+; the representation of knowledge in substitutions (part 2) into a 
+; logic system.
